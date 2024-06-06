266970b766dd6649da1a1975b7a8aae839193f9d
Peter Absolon <peterabsolon@yahoo.com>
2024-06-06 13:05:56 +0200

    chore: ✨ run checks on staged files only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
+  const foo = '3';
+  console.log({ foo });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home</h1>
