afa526eb94476ec88b13bc994de5bb3c6ed4e4f8
Peter Absolon <peterabsolon@yahoo.com>
2024-06-06 13:52:57 +0200

    test: 🧪 basic e2e test, cleanup

---
+++ e2e/app.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@playwright/test';
+
+const APP_URL = 'http://localhost:3000';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(APP_URL);
+});
+
+test.describe('app', () => {
+  test('whatever', async ({ page }) => {
+    await expect(page.getByText('Perspective.co')).toBeVisible();
+  });
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
-  const foo = '3';
-  console.log({ foo });
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+      <h1>Perspective.co</h1>
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
