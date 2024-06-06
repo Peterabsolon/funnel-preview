faed6c4a14d7271533189a65ce913d64356b1264
Peter Absolon <peterabsolon@yahoo.com>
2024-06-06 15:11:36 +0200

    config: 🔐 enable switch exhaustiveness check

--- src/components/blocks/ButtonBlock/ButtonBlock.tsx
+++
@@ -1,7 +0,0 @@
-import * as T from '~/types'
-
-export interface ButtonBlockProps extends T.ButtonBlock {}
-
-export const ButtonBlock = (props: ButtonBlockProps) => {
-  return <div>ButtonBlock</div>
-}
